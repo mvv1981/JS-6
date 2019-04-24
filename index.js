eea77c660161dd7b78ce14e55ba747f5a426dc51
Vlad Zhichkin <v.zhichkin@yandex.ru>
2019-04-24 21:24:11 +0400

    Zhichkin. Подсмотрел как у других и перенес в отдельные функции код. Добавил один юнит тест.

--- index.js
+++ index.js
@@ -10,54 +10,55 @@ module.exports = function (command) {
     var commandList = command.split(" ");
     var commandName = commandList.shift();
 
-    // Обработка команды ADD
-    if (commandName == 'ADD') {
-        var key = commandList.shift();
-
-        if (!phoneBook[key]) {
-            phoneBook[key] = {};
-        }
-        var stringNumbers = commandList.toString();
-        var arrayNumbers = stringNumbers.split(",");
-        for (i = 0; i < arrayNumbers.length; i++) {
-            var phoneNumber = arrayNumbers[i];
-            if (phoneNumber != "") {
-                phoneBook[key][phoneNumber] = true;
-            }
-        }
-        return true;
+    switch (commandName) {
+        case 'ADD':
+            return addContact(commandList);
+        case 'REMOVE_PHONE':
+            return removePhone(commandList);
+        case 'SHOW':
+            return showContacts();
+        default:
+            break;
     }
-
-    // Обработка команды REMOVE_PHONE
-    if (commandName == 'REMOVE_PHONE') {
-        var removePhone = false;
-        for (let key in phoneBook) {
-
-            if (commandList[0] in phoneBook[key]) {
-                delete phoneBook[key][commandList[0]];
-                removePhone = true;
-            }
-        }
-        return removePhone;
-    }
-
-    // Обработка команды SHOW
-    if (commandName === 'SHOW') {
-        var finalPhoneBook = [];
-        var tempPhoneBook = Object.keys(phoneBook).sort();
-        for (i = 0; i < tempPhoneBook.length; i++) {
-            var tZ = tempPhoneBook[i];
-            var tempValue = Object.keys(phoneBook[tZ]);
-            if (tempValue.toString().length > 1) {
-                finalPhoneBook.push(tempPhoneBook[i] + ": " + tempValue.join(', '));
-            } else {
-                tempPhoneBook.splice(i, 1);
-                i--;
-            }
-        }
-        return finalPhoneBook;
-    }
-
-
 }
+function addContact(parametr) {
+    var key = parametr.shift();
 
+    if (!phoneBook[key]) {
+        phoneBook[key] = {};
+    }
+    var stringNumbers = parametr.toString();
+    var arrayNumbers = stringNumbers.split(",");
+    for (i = 0; i < arrayNumbers.length; i++) {
+        var phoneNumber = arrayNumbers[i];
+        if (phoneNumber != "") {
+            phoneBook[key][phoneNumber] = true;
+        }
+    }
+    return true;
+}
+function removePhone(parametr) {
+    var removePhone = false;
+    for (let key in phoneBook) {
+        if (parametr[0] in phoneBook[key]) {
+            delete phoneBook[key][parametr[0]];
+            removePhone = true;
+        }
+    }
+    return removePhone;
+}
+function showContacts() {
+    var finalPhoneBook = [];
+    var tempPhoneBook = Object.keys(phoneBook).sort();
+    for (i = 0; i < tempPhoneBook.length; i++) {
+        var tZ = tempPhoneBook[i];
+        var tempValue = Object.keys(phoneBook[tZ]);
+        if (tempValue.toString().length > 1) {
+            finalPhoneBook.push(tempPhoneBook[i] + ": " + tempValue.join(', '));
+        } else {
+            tempPhoneBook.splice(i, 1);
+            i--;
+        }
+    }
+    return finalPhoneBook;
+}    
